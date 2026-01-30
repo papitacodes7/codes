88b1ddcf35764bede4145eecd85c726537e3ec29
Param <Parampc367@gmail.com>
2026-01-30 18:52:38 +0530

    Detect circular linked list by checking traversal back to head

--- DSA_2026/Linked List/1)Reverse a Linked List.cpp
+++ DSA_2026/Linked List/1)Reverse a Linked List.cpp
@@ -1,94 +1,53 @@
-class Node:
-    def __init__(self, newData):
-        self.data = newData
-        self.next = None
+#include <iostream>
+using namespace std;
 
-def reverseList(head):
+class Node {
+public:
+    int data;
+    Node* next;
 
-    curr = head
-    prev = None
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
 
-    # traverse all the nodes of Linked List
-    while curr is not None:
+Node* reverseList(Node* head) {
 
-        # store next
-        nextNode = curr.next
+    Node* curr = head;
+    Node* prev = NULL;
 
-        # reverse current node's next pointer
-        curr.next = prev
+    while (curr != NULL) {
 
-        # move pointers one position ahead
-        prev = curr
-        curr = nextNode
+        Node* nextNode = curr->next; // store next
+        curr->next = prev;           // reverse link
+        prev = curr;                 // move prev
+        curr = nextNode;             // move curr
+    }
 
-    return prev
+    return prev; // new head
+}
 
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data;
+        if (node->next != NULL)
+            cout << " -> ";
+        node = node->next;
+    }
+    cout << endl;
+}
 
-def printList(node):
-    while node is not None:
-        print(f"{node.data}", end="")
-        if node.next is not None:
-            print(" -> ", end="")
-        node = node.next
-    print()
+int main() {
 
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
 
-if __name__ == "__main__":
+    head = reverseList(head);
+    printList(head);
 
-    head = Node(1)
-    head.next = Node(2)
-    head.next.next = Node(3)
-    head.next.next.next = Node(4)
-    head.next.next.next.next = Node(5)
-    head = reverseList(head)
-    printList(head)
-
-
-
-    class Node:
-    def __init__(self, newData):
-        self.data = newData
-        self.next = None
-
-def reverseList(head):
-    if head is None or head.next is None:
-        return head
-
-    # reverse the rest of linked list and put the
-    # first element at the end
-    rest = reverseList(head.next)
-
-    # make the current head as last node of
-    # remaining linked list
-    head.next.next = head
-
-    # update next of current head to NULL
-    head.next = None
-
-    return rest
-
-
-def printList(node):
-    while node is not None:
-        print(f"{node.data}", end="")
-        if node.next is not None:
-            print(" -> ", end="")
-        node = node.next
-    print()
-
-
-if __name__ == "__main__":
-
-    # Create a hard-coded linked list:
-    # 1 -> 2 -> 3 -> 4 -> 5
-    head = Node(1)
-    head.next = Node(2)
-    head.next.next = Node(3)
-    head.next.next.next = Node(4)
-    head.next.next.next.next = Node(5)
-
-    head = reverseList(head)
-    printList(head)
-
-
-    
+    return 0;
+}
