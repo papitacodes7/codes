afd3f5022f6dc60b61ee8239da76be08ec329b6c
Param <Parampc367@gmail.com>
2026-04-01 22:12:00 +0530

    test

---
+++ DSA_2026/Graphs/12)minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
+        
+    }
+};
+
+// tomorrow do from https://youtu.be/8gYBHjtjWBI?si=QWsLBBU6E9MQERgX 11:20
